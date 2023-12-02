b4f5ad3e9e5025b55177c40770c033e0457c34a6
Marius Hennecke <marius.hennecke@vorwerk.de>
2023-12-02 14:46:38 +0100

    2023 day 2

---
+++ 2023/2.1.py
@@ -0,0 +1,44 @@
+import os
+import parse
+import operator
+from functools import reduce
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+input_file = os.path.join(script_dir, f'{os.path.basename(__file__).split(".")[0]}.input')
+test_input_file = os.path.join(script_dir, f'{os.path.basename(__file__).split(".")[0]}.test.input')
+
+
+def read_data(file_name: str) -> list[dict]:
+    games = []
+    with open(file_name, 'r') as f:
+        for line in f:
+            line = line.strip()
+            g = line.split(': ', 1)[1]
+            rounds = []
+            for round in g.split('; '):
+                r = {}
+                for color in round.split(', '):
+                    c_value, c_name = parse.parse('{:d} {:w}', color)
+                    r[c_name] = c_value
+                rounds.append(r)
+            games.append(rounds)
+
+    return games
+
+
+def max_cubes(game: list[dict[int]]) -> dict[int]:
+    m = {'red': 0, 'green': 0, 'blue': 0}
+    for draw in game:
+        m = {c: max(m[c], draw.get(c, 0)) for c in m}
+    return m
+
+
+test_games = read_data(test_input_file)
+assert max_cubes(test_games[0]) == {'red': 4, 'green': 2, 'blue': 6}
+assert max_cubes(test_games[1]) == {'red': 1, 'green': 3, 'blue': 4}
+assert max_cubes(test_games[2]) == {'red': 20, 'green': 13, 'blue': 6}
+assert max_cubes(test_games[3]) == {'red': 14, 'green': 3, 'blue': 15}
+assert max_cubes(test_games[4]) == {'red': 6, 'green': 3, 'blue': 2}
+
+games = read_data(input_file)
+print(reduce(operator.add, map(lambda game: reduce(operator.mul, max_cubes(game).values(), 1), games), 0))
---
+++ 2023/2.py
@@ -0,0 +1,45 @@
+import os
+import parse
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+input_file = os.path.join(script_dir, f'{os.path.basename(__file__).split(".")[0]}.input')
+test_input_file = os.path.join(script_dir, f'{os.path.basename(__file__).split(".")[0]}.test.input')
+
+
+def read_data(file_name: str) -> list[dict]:
+    games = []
+    with open(file_name, 'r') as f:
+        for line in f:
+            line = line.strip()
+            g = line.split(': ', 1)[1]
+            rounds = []
+            for round in g.split('; '):
+                r = {}
+                for color in round.split(', '):
+                    c_value, c_name = parse.parse('{:d} {:w}', color)
+                    r[c_name] = c_value
+                rounds.append(r)
+            games.append(rounds)
+
+    return games
+
+
+def valid_draw(draw: dict[int], bag: dict[int]) -> bool:
+    return all([k in bag and v <= bag[k] for k, v in draw.items()])
+
+
+def valid_game(game: list[dict[int]], bag: dict[int]) -> bool:
+    return all([valid_draw(g, bag) for g in game])
+
+
+test_games = read_data(test_input_file)
+bag = {'red': 12, 'green': 13, 'blue': 14}
+
+assert valid_game(test_games[0], bag)
+assert valid_game(test_games[1], bag)
+assert not valid_game(test_games[2], bag)
+assert not valid_game(test_games[3], bag)
+assert valid_game(test_games[4], bag)
+
+games = read_data(input_file)
+print(sum([i + 1 if valid_game(game, bag) else 0 for i, game in enumerate(games)]))
