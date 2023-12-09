d57ed2f4e5dfa565563de06dadfc09158d3d4f79
Marius Hennecke <marius.hennecke@vorwerk.de>
2023-12-09 10:33:20 +0100

    2023 day 9

---
+++ 2023/9-mirage/part1.py
@@ -0,0 +1,42 @@
+import os
+from itertools import pairwise
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+input_file = os.path.join(script_dir, 'input')
+test_input_file = os.path.join(script_dir, 'test.input')
+
+
+def read_data(file_name: str) -> list[list[int]]:
+    data = []
+    with open(file_name, 'r') as f:
+        for line in f:
+            data.append([int(m) for m in line.strip().split(' ')])
+    return data
+
+
+def oasis_extrapolate(measurement: list[int]) -> int:
+    triag = [measurement]
+
+    for i in range(len(measurement) - 1):
+        triag.append([m2 - m1 for m1, m2 in pairwise(triag[-1])])
+        if all([t == 0 for t in triag[-1]]):
+            break
+    e = 0
+    for i in range(len(triag) - 2, -1, -1):
+        e += triag[i][-1]
+    return e
+
+
+def sum_oasis_extrapolation(measuremenmts: list[list[int]]):
+    return sum(map(oasis_extrapolate, measuremenmts))
+
+
+test_data = read_data(test_input_file)
+assert oasis_extrapolate(test_data[0]) == 18
+assert oasis_extrapolate(test_data[1]) == 28
+assert oasis_extrapolate(test_data[2]) == 68
+assert sum_oasis_extrapolation(test_data) == 114
+
+
+data = read_data(input_file)
+print(sum_oasis_extrapolation(data))
