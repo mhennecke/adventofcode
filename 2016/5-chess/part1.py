9046e81a91ffee7a8789dae90b260cde13cc1ff8
Marius Hennecke <marius.hennecke@vorwerk.de>
2024-02-18 19:29:17 +0100

    2016 day 5

---
+++ 2016/5-chess/part1.py
@@ -0,0 +1,27 @@
+import os
+from hashlib import md5
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+input_file = os.path.join(script_dir, 'input')
+test_input_file = os.path.join(script_dir, 'test.input')
+
+
+def read_data(file_name: str) -> str:
+    return open(file_name, 'r').readline().strip()
+
+
+def password(door_id: str) -> int:
+    p = []
+    p_i = 0
+    i = 0
+    while p_i < 8:
+        h = md5((door_id + str(i)).encode()).hexdigest()
+        if h[:5] == '00000':
+            p.append(h[5])
+            p_i += 1
+        i += 1
+    return str.join('', p)
+
+assert password(read_data(test_input_file)) == '18f47a30'
+
+print(password(read_data(input_file)))
