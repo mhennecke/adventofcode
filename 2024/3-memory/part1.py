2a6cbc0e71c0fa723a5953fbaa9a44ae42b1fcc3
Marius Hennecke <marius.hennecke@de.abb.com>
2024-12-03 22:34:59 +0100

    2024 day 3

---
+++ 2024/3-memory/part1.py
@@ -0,0 +1,25 @@
+import os
+import re
+from functools import reduce
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+input_file = os.path.join(script_dir, 'input')
+test_input_file = os.path.join(script_dir, 'test.input')
+
+
+def read_data(file_name: str) -> str:
+    with open(file_name, 'r') as f:
+        return f.read().strip().replace('\n', '')
+
+
+def mul_corrupted(memory: str) -> bool:
+    matches = re.findall(r"mul\((\d+),(\d+)\)", memory)
+    return reduce(lambda x, m: x + int(m[0]) * int(m[1]), matches, 0)
+
+
+test_data = read_data(test_input_file)
+assert mul_corrupted(test_data) == 161
+
+
+d = mul_corrupted(read_data(input_file))
+print(d)
